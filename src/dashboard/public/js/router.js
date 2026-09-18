04056d3a0cdeb83ba86e8c58720730626f1ebf14
AbuZar <abuzarbabar5@gmail.com>
2026-09-18 13:58:10 +0500

    feat: initialize visual workflow editor dashboard with drawflow, router, and data store

--- src/dashboard/public/js/router.js
+++ src/dashboard/public/js/router.js
@@ -5,8 +5,6 @@
 import { OverviewView } from './views/overviewView.js?v=5';
 import { WorkflowsView } from './views/workflowsView.js?v=5';
 import { ConsoleView } from './views/consoleView.js?v=5';
-import { PortalsView } from './views/portalsView.js?v=5';
-import { TestsView } from './views/testsView.js?v=5';
 import { SecretsView } from './views/secretsView.js?v=5';
 import { ArtifactsView } from './views/artifactsView.js?v=5';
 import { WorkflowEditorView } from './views/workflowEditorView.js?v=5';
@@ -22,8 +20,6 @@ export const Router = {
     overview: OverviewView,
     workflows: WorkflowsView,
     console: ConsoleView,
-    portals: PortalsView,
-    tests: TestsView,
     secrets: SecretsView,
     artifacts: ArtifactsView,
     'workflow-editor': WorkflowEditorView
